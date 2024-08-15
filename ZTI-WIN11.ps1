ba2e568f43d2d4d0b9e8aaeafde9558c15882ac7
MPT-Spearsie <kspear@managepoint.ca>
2024-08-14 20:36:05 -0400

    Update ZTI-WIN11.ps1
    Updated OSLicense edition from "Volume" to "Retail"

--- ZTI-WIN11.ps1
+++ ZTI-WIN11.ps1
@@ -1,7 +1,7 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail -ZTI
+Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Retail -OSEdition Pro -ZTI
 
 #Restart from WinPE
 
