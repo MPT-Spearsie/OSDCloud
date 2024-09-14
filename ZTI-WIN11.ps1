0d07da5eb05250751a152d6e50cacec1c84fa56a
MPT-Spearsie <kspear@managepoint.ca>
2024-09-14 08:43:45 -0400

    Update ZTI-WIN11.ps1

--- ZTI-WIN11.ps1
+++ ZTI-WIN11.ps1
@@ -1,24 +1,24 @@
 # Get all removable devices
-$removableDevices = Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE DriveType = 2"
+##$removableDevices = Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE DriveType = 2"
 
 # Create a Shell.Application COM object
-$shell = New-Object -ComObject Shell.Application
+##$shell = New-Object -ComObject Shell.Application
 
 # Eject each removable device
-foreach ($device in $removableDevices) {
-    $driveLetter = $device.DriveLetter
-    if ($driveLetter) {
-        $shell.Namespace(17).ParseName($driveLetter).InvokeVerb("Eject")
-        Write-Output "Ejected: $driveLetter"
-    }
-}
-Start-Sleep -Seconds 5
+##foreach ($device in $removableDevices) {
+##    $driveLetter = $device.DriveLetter
+##    if ($driveLetter) {
+##        $shell.Namespace(17).ParseName($driveLetter).InvokeVerb("Eject")
+##        Write-Output "Ejected: $driveLetter"
+##    }
+##}
+##Start-Sleep -Seconds 5
 
 # Start OSDCloud ZTI
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Retail -OSEdition Pro -ZTI
+Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 22H2 x64' -OSLicense Retail -OSEdition Pro -ZTI -ErrorAction Continue
 
 #Restart from WinPE
 
