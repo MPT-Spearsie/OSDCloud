996f4e02a3976cae112fd96ff685c88bc8504f3f
MPT-Spearsie <kspear@managepoint.ca>
2024-09-15 11:33:15 -0400

    Update ZTI-VENTOY.ps1

--- ZTI-VENTOY.ps1
+++ ZTI-VENTOY.ps1
@@ -1,5 +1,5 @@
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
-$OSName = 'Windows 11 23H2 x64'
+$OSName = 'Windows 11 22H2 x64'
 $OSEdition = 'Pro'
 $OSActivation = 'Retail'
 $OSLanguage = 'en-us'
