c5e94e61ac2c7cebd9c062a4fa06ed8b890a0258
MPT-Spearsie <kspear@managepoint.ca>
2024-09-15 12:17:25 -0400

    Update ZTI-VENTOY.ps1

--- ZTI-VENTOY.ps1
+++ ZTI-VENTOY.ps1
@@ -6,17 +6,7 @@ $OSLanguage = 'en-us'
 
 #Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
-    RecoveryPartition = [bool]$true
-    OEMActivation = [bool]$True
-    WindowsUpdate = [bool]$true
-    WindowsUpdateDrivers = [bool]$true
-    WindowsDefenderUpdate = [bool]$true
-    SetTimeZone = [bool]$true
-    ClearDiskConfirm = [bool]$False
-    ShutdownSetupComplete = [bool]$false
-    SyncMSUpCatDriverUSB = [bool]$true
-    CheckSHA1 = [bool]$true
+    KeepUSBDrive = $true
 }
 
 Write-Output $Global:MyOSDCloud
