720f39e27b631ca6e6862a62e9a8495bac4d4caa
MPT-Spearsie <kspear@managepoint.ca>
2026-04-07 08:55:20 -0400

    Update ZTI-WIN11.ps1

--- ZTI-WIN11.ps1
+++ ZTI-WIN11.ps1
@@ -2,8 +2,8 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSLicense Retail -OSEdition Pro -ZTI
-
+#Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSLicense Retail -OSEdition Pro -ZTI
+Start-OSDCloudGUI
 #Restart from WinPE
 
 Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
